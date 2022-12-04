b67bc729f9910fe9212a2c77decada6f74dfa60f
EliaBrambilla <elia.brambilla@icloud.com>
2022-12-04 21:01:37 +0100

    aggiorno sezione 5 e 6

---
+++ src/components/Section5Comp.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="section5">
+        <div class="sec5img">
+            <img v-for='(elem,index) in Section5images' :key="index" :src="require(`../assets/img/${elem.path}`) " alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Section5Comp',
+    components: {
+    },
+    props: {
+        Section5images: Array
+    },
+    data() {
+        return {
+        }
+    },
+    mounted() {
+    },
+    methods: {
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.section5 {
+    
+    width: calc(100% / 4);
+    .sec5img{
+        display:flex;
+        img{
+        width: 100%;
+        }
+    }
+    
+}
+</style>
---
+++ src/components/Section6Comp.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="section6">
+        
+        <div class="sec6img">
+            <img v-for='(elem,index) in Section6Logos' :key="index" :src="require(`../assets/img/${elem.path}`) " alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Sectio6Comp',
+    components: {
+    },
+    props: {
+        Section6Logos: Array
+    },
+    data() {
+        return {
+        }
+    },
+    mounted() {
+    },
+    methods: {
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.section6 {
+    padding:50px 40px;
+    width: calc(100% / 4);
+    .sec6img {
+        display: flex;
+        img {
+            width: 100%;
+        }
+    }
+}
+</style>
