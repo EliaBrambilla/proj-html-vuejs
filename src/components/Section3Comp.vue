8861f2a08ac63548d7e7e31a3b9992ebd807b59d
EliaBrambilla <elia.brambilla@icloud.com>
2022-12-04 21:00:44 +0100

    aggiorno sezione 3 e 4

---
+++ src/components/Section3Comp.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="section3">
+        <div class="sec3Content">
+            <div class="sec3Img">
+             <img src="../assets/img/h1-img-7n.png" alt="">   
+            </div>
+             <div class="sec3Text">
+                <ul>
+                    <li>
+                        <div class="specials">SPECIALS*</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ut.</p> 
+                    </li>
+                    <li v-for="elem,index in  Section3Specials" :key="index" class="comboCard">
+                        <div class="comboItem" >${{elem.price}}</div>
+                        <div>
+                           <div>{{elem.name}}</div>
+                           <p>{{elem.description}}</p>
+                        </div>
+                    </li>
+                </ul> 
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+     export default {
+        name:'Section3Comp',
+        components:{
+        },
+        props:{
+        Section3Specials:Array
+        },
+        data(){
+            return{
+            }
+        },
+        mounted(){
+        },
+        methods:{
+            
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.section3{
+    
+    .sec3Content{
+        display: flex;
+        
+        .sec3Img{
+            flex-basis: 50%;
+            background-image: url('../assets/img/h1-img-4.jpg');
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            display: flex;
+            justify-content: center;
+            img{
+            padding: 50px;
+            width: 80%;
+            }
+        }
+        .sec3Text{
+            flex-basis: 50%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            .specials{
+                font-weight: bold;
+                font-size: 2rem;
+                
+            }
+            .comboCard{
+                display: flex;
+                padding-top: 30px;
+                .comboItem{
+                    padding-right: 20px;
+                }
+               
+            }   
+        }
+        
+        
+        
+    }
+}
+</style>
---
+++ src/components/Section4Comp.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="section4">
+        <div class="sec4Text">
+          <h2>GO AHEAD AND</h2>
+          <h2>BUILD YOUR </h2>
+          <h2>OWN PIZZA </h2>
+          <h2>WE WON'T </h2>
+          <h2>JUDJE!</h2>  
+        </div>
+      
+    </div>
+</template>
+
+<script>
+     export default {
+        name:'Section4Comp',
+        components:{
+        },
+        props:{
+        },
+        data(){
+            return{
+            }
+        },
+        mounted(){
+        },
+        methods:{
+            
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.section4{
+    display: flex;
+    
+    
+    height: 80vh;
+    background-image:url('../assets/img/h3-background-img.jpg');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    .sec4Text{
+    line-height: 4.5rem;    
+    font-family: 'Oswald', sans-serif;
+    margin: 50px 300px;    
+    font-size: 3rem;       
+    //inline-size: 450px;
+    //overflow-wrap: break-word;
+    color:white;
+    }
+}
+</style>
