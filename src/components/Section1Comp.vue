1b163f84bd09aff5e3a5f4a8be36caf0efd5f83f
EliaBrambilla <elia.brambilla@icloud.com>
2022-12-04 20:59:54 +0100

    aggiorno sezione 1 e 2 vue

---
+++ src/components/Section1Comp.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="section1">
+     <div class="sec1img">
+        <img v-for='(elem,index) in Section1images' :key="index" :src="require(`../assets/img/${elem.path}`) " alt="">
+     </div>
+    </div>
+</template>
+
+<script>
+     export default {
+        name:'Section1Comp',
+        components:{
+        },
+        props:{
+        Section1images:Array
+        },
+        data(){
+            return{
+            }
+        },
+        mounted(){
+        },
+        methods:{
+            
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.section1 {
+    
+    width: calc(100% / 4);
+    .sec1img{
+        display:flex;
+        img{
+        width: 100%;
+        }
+    }
+    
+}
+</style>
---
+++ src/components/Section2Comp.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="section2">
+      <div class="section2-text">
+       <div class="quote" ><font-awesome-icon icon="fa-solid fa-quote-left" /></div>
+       <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto eveniet incidunt sed excepturi</h1> 
+      </div>
+      
+
+    </div>
+</template>
+
+<script>
+     export default {
+        name:'Section2Comp',
+        components:{
+        },
+        props:{
+        },
+        data(){
+            return{
+            }
+        },
+        mounted(){
+        },
+        methods:{
+            
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.section2{
+    height: 40vh;
+    
+    background-image: url('../assets/img/h3-testimonials-bckgrnd.jpg');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    .section2-text{
+        width: 60%;
+        margin: 0 auto;
+        .quote{
+             text-align: center;
+             font-size: 4rem;
+        }
+       h1{
+       
+        text-align: center;
+    } 
+    } 
+    
+}
+</style>
