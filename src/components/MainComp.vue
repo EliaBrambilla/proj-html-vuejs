00602df00b181b2fca87995dd7b9d6e733d60bd4
EliaBrambilla <elia.brambilla@icloud.com>
2022-12-04 20:59:00 +0100

    aggiorno main vue

---
+++ src/components/MainComp.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+     export default {
+        name:'MainComp',
+        components:{
+        },
+        props:{
+        },
+        data(){
+            return{
+            }
+        },
+        mounted(){
+        },
+        methods:{
+            
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
