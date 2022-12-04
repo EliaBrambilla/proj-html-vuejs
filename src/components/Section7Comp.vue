3de87fc6000e5915369a272f18f58dbdae383be1
EliaBrambilla <elia.brambilla@icloud.com>
2022-12-04 21:02:14 +0100

    aggiorno sezione 7 e 8

---
+++ src/components/Section7Comp.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="section7">
+        <div class="sec7Text">
+            <h6>CHOOSE YOUR FLAVOUR</h6>
+            <h2>THE BEST PIZZA MENU IN TOWN</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae cupiditate mollitia</p>
+        </div>
+        <div  class="sec7Card">
+            <div v-for='(elem,index) in Section7images' :key="index"  class="sec7Content">
+              <div class="pizza">
+                <img :src="require(`../assets/img/${elem.path}`) " alt="">
+                <div v-if="elem.discountBadge"  class="discount"> {{elem.discount}}%</div>
+                <div v-if="elem.sold"  class="sold">SOLD</div>
+              </div>
+                <div>{{elem.name}}</div>
+                <div class="price">
+                <div :class="(elem.discountBadge == true ) ?  'priceGrey' : 'priceRed'">${{elem.price}}</div>
+                <div class="priceRed" v-if="elem.discountBadge" >${{discountPrice(elem)}}</div>   
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Section7Comp',
+    components: {
+    },
+    props: {
+        Section7images: Array
+    },
+    data() {
+        return {
+        
+        }
+    },
+    mounted() {
+    //this.discountPrice()
+    },
+    methods: {
+    discountPrice(pizza){
+        //console.log(parseInt(pizza.price))
+        //console.log(parseInt(pizza.discount))
+        let discountedPrice =  parseInt(pizza.price) - (parseInt(pizza.price) * parseInt(pizza.discount) / 100 ) 
+        console.log(discountedPrice)
+        return discountedPrice
+    
+       
+      
+      
+    }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.section7 {
+    .sec7Text {
+      padding: 50px 0px;
+        text-align: center;
+        font-family: 'Oswald', sans-serif;
+        h6 {
+            color: #d7573c;
+        }
+    }
+    .sec7Card {
+       padding: 50px 0px;
+        display: flex;
+        text-align: center;
+        .price{
+         display: flex;
+         justify-content: center;   
+        .priceRed{
+            color: red;
+        }
+        .priceGrey{
+            color: grey;
+            text-decoration: line-through;
+        }   
+        }
+        .sec7Content{
+           width: calc(100% / 6);
+          img {
+            width: 100%;
+           }
+           .pizza{
+            position: relative;
+           .discount,.sold{
+            color: white;
+            position: absolute;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 60px;
+            height:60px;
+            border-radius: 50%;
+           }
+           .discount{
+           top: 10px;
+           background-color: red;
+           }
+           .sold{
+            right: 0px;
+            top: 10px;
+            background-color:brown;
+           }
+           }
+        }
+        
+    }
+}
+</style>
---
+++ src/components/Section8Comp.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="section8">
+        <div class="sec8Content">
+            <div class="sec8Text">
+                <!-- card -->
+                 <ul>
+                    <li>made whith love</li>
+                    <li class="cardTitle" > DELISH PIZZA DEALS </li>
+                    <li v-for="element,index in Section8Events" :key='index' class="eventCard">
+                        <div class="left" >
+                            <div class="eventDay" >{{element.day}}</div>
+                            <div class="eventTitle" >{{element.month}}</div>
+                        </div>
+                        <div>
+                            <div class="eventTitle" >{{element.title}}</div>
+                            <div>{{element.geoTag}}</div>
+                        </div>
+                    </li>
+                 </ul>
+            </div>
+            <div class="sec8Img"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Section8Comp',
+    components: {
+    },
+    props: {
+    Section8Events:Array
+    },
+    data() {
+        return {
+        }
+    },
+    mounted() {
+    },
+    methods: {
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.section8 {
+    .sec8Content {
+        display: flex;
+         .sec8Text {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-image: url('../assets/img/sky-1475274047050-1d0c0975c63e.jpeg');
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            flex-basis: 50%;
+            ul{
+                    .cardTitle{
+                    font-size: 2rem;
+                    font-weight: bold;
+                }
+                li{
+                color: white;
+                text-align: start;
+ 
+                }
+                .eventCard{
+                display: flex;
+                align-items: center;
+                color: white;
+                border-bottom: 1px dotted white;
+                .left{
+                    padding-right: 20px;
+                }
+                .eventDay{
+                    font-size: 2rem;
+                    font-weight: bold;
+                }
+                .eventTitle{
+                    font-size: 1.2rem;
+                    font-weight: bold;
+                }
+                
+                }
+            }
+            h2,H3 {
+                color: white;
+                text-align: center;
+            }
+        }
+        .sec8Img {
+            height: 90vh;
+            flex-basis: 50%;
+            background-image: url('../assets/img/h3-img-5a.jpg');
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            display: flex;
+            justify-content: center;
+            img {
+                padding: 50px;
+                width: 80%;
+            }
+        }
+       
+    }
+}
+</style>
